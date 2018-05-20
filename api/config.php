561a0c93660ce1f1e6fb702c0e14c9172a6d5544
therealDaze <danieldahlman@outlook.com>
2018-05-20 19:14:05 +0200

    added code to api dir files api, config, functions, index, resource

---
+++ api/config.php
@@ -0,0 +1,19 @@
+<?php
+/* $dbusername = "219307_ot65650";
+$dbhost = "danieldahlman-219307.mysql.binero.se";
+$dbpassword = "Leinad0243";
+$db = "219307-danieldahlman";
+*/
+$dbusername = "root";
+$dbhost = "localhost";
+$dbpassword = "";
+$db = "test";
+
+
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+
+
+$connection = mysqli_connect($dbhost, $dbusername, $dbpassword, $db);
+mysqli_query($connection, "SET NAMES utf8");
+?>
