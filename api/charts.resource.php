7c425ca47c00e501608c7d4d5a8ece6ce15f827f
therealDaze <danieldahlman@outlook.com>
2018-06-26 19:03:00 +0200

    removed comments from chart.resource.php

--- api/charts.resource.php
+++ api/charts.resource.php
@@ -1,59 +1,26 @@
 <?php
-#
-# Den här klassen ska köras om vi anropat resursen user i vårt API genom /?/user
-#
-class _charts extends Resource{ // Klassen ärver egenskaper från den generella klassen Resource som finns i resource.class.php
-    # Här deklareras de variabler/members som objektet ska ha
+
+class _charts extends Resource{
     public $chart, $id, $ordernumber, $report, $charttype, $cssclass, $x, $y, $slotpositions, $request;
-    # Här skapas konstruktorn som körs när objektet skapas
+    
     function __construct($resource_id, $request){
-        
-        # Om vi fått med ett id på resurser (Ex /?/user/15) och det är ett nummer sparar vi det i objektet genom $this->id
         if(is_numeric($resource_id))
         $this->id = $resource_id;
-        # Vi sparar också det som kommer med i URL:en efter vårt id som en array
         $this->request = $request;
     }
-    # Denna funktion körs om vi anropat resursen genom HTTP-metoden GET
+    
     function GET($input, $connection){
         $result = mysqli_query($connection, "SELECT id, ordernumber, report, charttype, cssclass FROM charts");
         $chart = [];
         while($row = mysqli_fetch_assoc($result)) {
             $chart[] = $row;
-            /*  $slot_array = explode(" | ", $row['slotpositions']);
-            $slot = [];
-            foreach ($slot_array as $item) {
-            list($xPos,$yPos) = explode(", ", $item);
-            $slot[] = ['xPos' => (int)$xPos, 'yPos' => (int)$yPos];
-            }
-            
-            $chart[] = [
-            'id' => $row['id'],
-            'ordernumber' => $row['ordernumber'],
-            'report' => $row['report'],
-            'charttype' => $row['charttype'],
-            'cssclass' => $row['cssclass'],
-            'x' => $row['x'],
-            'y' =>  $row['y'],
-            'slotpositions' => $slot
-            ]; */
         }
-        
         $this->chart = $chart;
     }
-    # Denna funktion körs om vi anropat resursen genom HTTP-metoden POST
+    
     function POST($input, $connection){
-        /* $ordernumber = escape($input['ordernumber']);
-        $report = escape($input['report']);
-        $charttype = escape($input['charttype']);
-        $cssclass = escape($input['cssclass']); */
-        /*   $x = escape($input['x']);
-        $y = escape($input['y']);
-        $slotpositions = implode_array($input['slotpositions']);
-        $slotpositions = escape($slotpositions); */
         
         $chartdata = $input['chartdata'];
-        
         $chart = [];
         foreach ($chartdata as $key) {
             $ordernumber = escape($key['ordernumber']);
@@ -74,21 +41,14 @@ class _charts extends Resource{ // Klassen ärver egenskaper från den generella
                 ];
             }
         }
-        
         $this->chart = $chart;
     }
-    # Denna funktion körs om vi anropat resursen genom HTTP-metoden PUT
     function PUT($input, $connection){
         $id = escape($input['id']);
         $ordernumber = escape($input['ordernumber']);
         $report = escape($input['report']);
         $charttype = escape($input['charttype']);
         $cssclass = escape($input['cssclass']);
-        /*   $x = escape($input['x']);
-        $y = escape($input['y']);
-        
-        $slotpositions = implode_array($input['slotpositions']);
-        $slotpositions = escape($slotpositions); */
         
         $query = "UPDATE charts
         SET ordernumber = '$ordernumber',
@@ -103,14 +63,9 @@ class _charts extends Resource{ // Klassen ärver egenskaper från den generella
             $this->report = $report;
             $this->charttype = $charttype;
             $this->cssclass = $cssclass;
-            /*      $this->x = $x;
-            $this->y = $y;
-            $this->slotpositions = $slotpositions; */
         }
     }
-    # Denna funktion körs om vi anropat resursen genom HTTP-metoden DELETE
     function DELETE($input, $connection){
-        # I denna funktion tar vi bort en specifik user med det ID vi fått med
         if($this->id){
             $query = "DELETE FROM charts
             WHERE id = $this->id";
