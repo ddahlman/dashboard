3b170c498c2939b8aa7fb6f8b460888f13539968
therealDaze <danieldahlman@outlook.com>
2018-06-26 19:10:31 +0200

    cleaned reports.resource.php

--- api/functions.php
+++ api/functions.php
@@ -93,12 +93,4 @@ function chart_options($result) {
     ];
 }
 
-function implode_array($inputs) {
-    $implodedSlots = array_map(function($a){
-        return implode(", ", $a);
-    }, $inputs);
-    $slotpositions = implode(" | ", $implodedSlots);
-    return $slotpositions;
-}
-
 ?>
--- api/reports.resource.php
+++ api/reports.resource.php
@@ -1,22 +1,15 @@
 <?php
-#
-# Den här klassen ska köras om vi anropat resursen user i vårt API genom /?/user
-#
-class _reports extends Resource{ // Klassen ärver egenskaper från den generella klassen Resource som finns i resource.class.php
-    # Här deklareras de variabler/members som objektet ska ha
+
+class _reports extends Resource{
     public $reports, $request;
-    # Här skapas konstruktorn som körs när objektet skapas
+    
     function __construct($resource_id, $request){
-        
-        # Om vi fått med ett id på resurser (Ex /?/user/15) och det är ett nummer sparar vi det i objektet genom $this->id
         if(is_numeric($resource_id))
         $this->id = $resource_id;
-        # Vi sparar också det som kommer med i URL:en efter vårt id som en array
         $this->request = $request;
     }
-    # Denna funktion körs om vi anropat resursen genom HTTP-metoden GET
+    
     function GET($input, $connection){
-        // om vår URL inte innehåller ett ID hämtas alla users
         $result_bok = mysqli_query($connection, "SELECT * FROM bookings");
         $result_nat = mysqli_query($connection, "SELECT * FROM nationalities");
         $result_sale = mysqli_query($connection, "SELECT * FROM sale");
@@ -34,7 +27,6 @@ class _reports extends Resource{ // Klassen ärver egenskaper från den generell
         
         $bok_data = array();
         $bok_data["cols"] = array(
-        //Labels for the chart, these represent the column titles
         array("id" => "", "label" => "Biljett", "type" => "string"),
         array("id" => "", "label" => "Antal bokningar", "type" => "number")
         );
@@ -42,7 +34,6 @@ class _reports extends Resource{ // Klassen ärver egenskaper från den generell
         $bok_rows = array();
         foreach ($result_bok as $bok_row) {
             $bok_temp = array();
-            //Values
             $bok_temp[] = array("v" => (string) $bok_row["ticket_v"]);
             $bok_temp[] = array("v" => (int) $bok_row["bookings_v"]);
             $bok_temp[] = array("f" => (string) $bok_row["tooltip_f"]);
@@ -52,7 +43,6 @@ class _reports extends Resource{ // Klassen ärver egenskaper från den generell
         
         $nat_data = array();
         $nat_data["cols"] = array(
-        //Labels for the chart, these represent the column titles
         array("id" => "", "label" => "Land", "type" => "string"),
         array("id" => "", "label" => "Antal", "type" => "number")
         );
@@ -60,7 +50,6 @@ class _reports extends Resource{ // Klassen ärver egenskaper från den generell
         $nat_rows = array();
         foreach ($result_nat as $nat_row) {
             $nat_temp = array();
-            //Values
             $nat_temp[] = array("v" => (string) $nat_row["country_v"]);
             $nat_temp[] = array("v" => (int) $nat_row["number_v"]);
             $nat_temp[] = array("f" => (string) $nat_row["tooltip_f"]);
@@ -70,7 +59,6 @@ class _reports extends Resource{ // Klassen ärver egenskaper från den generell
         
         $sale_data = array();
         $sale_data["cols"] = array(
-        //Labels for the chart, these represent the column titles
         array("id" => "", "label" => "År", "type" => "string"),
         array("id" => "", "label" => "Kort", "type" => "number"),
         array("id" => "", "label" => "Faktura", "type" => "number")
@@ -79,7 +67,6 @@ class _reports extends Resource{ // Klassen ärver egenskaper från den generell
         $sale_rows = array();
         foreach ($result_sale as $sale_row) {
             $sale_temp = array();
-            //Values
             $sale_temp[] = array("v" => (string) $sale_row["year_v"]);
             $sale_temp[] = array("v" => (int) $sale_row["credit_card_v"]);
             $sale_temp[] = array("v" => (int) $sale_row["bill_v"]);
@@ -89,7 +76,6 @@ class _reports extends Resource{ // Klassen ärver egenskaper från den generell
         
         $tod_data = array();
         $tod_data["cols"] = array(
-        //Labels for the chart, these represent the column titles
         array("id" => "", "label" => "Land", "type" => "string"),
         array("id" => "", "label" => "Män", "type" => "number"),
         array("id" => "", "label" => "Kvinnor", "type" => "number")
@@ -98,7 +84,6 @@ class _reports extends Resource{ // Klassen ärver egenskaper från den generell
         $tod_rows = array();
         foreach ($result_tod as $tod_row) {
             $tod_temp = array();
-            //Values
             $tod_temp[] = array("v" => (string) $tod_row["country_v"]);
             $tod_temp[] = array("v" => (int) $tod_row["men_v"]);
             $tod_temp[] = array("v" => (int) $tod_row["women_v"]);
@@ -115,49 +100,5 @@ class _reports extends Resource{ // Klassen ärver egenskaper från den generell
         
         $this->reports = $obj;
         
-    }
-    # Denna funktion körs om vi anropat resursen genom HTTP-metoden POST
-    function POST($input, $connection){
-        # I denna funktion skapar vi en ny user med den input vi fått
-        $item = escape($input['item']);
-        $headersID = escape($input['headersID']);
-        
-        $query = "INSERT INTO items (headersID, list_item)
-        VALUES ('$headersID', '$item')";
-        
-        if(mysqli_query($connection, $query)) {
-            $this->headersID = $headersID;
-            $this->item = $item;
-        }
-    }
-    # Denna funktion körs om vi anropat resursen genom HTTP-metoden PUT
-    function PUT($input, $connection){
-        # I denna funktion uppdateras en specifik user med den input vi fått
-        # Observera att allt uppdaterad varje gång och att denna borde byggas om så att bara det vi skickar med uppdateras
-        if($this->id){
-            $item = escape($input['item']);
-            
-            $query = "UPDATE items
-            SET list_item = '$item'
-            WHERE id = $this->id
-            ";
-            
-            if(mysqli_query($connection, $query)) {
-                $this->item = $item;
-            }
-        }else{
-            echo "No resource given";
-        }
-    }
-    # Denna funktion körs om vi anropat resursen genom HTTP-metoden DELETE
-    function DELETE($input, $connection){
-        # I denna funktion tar vi bort en specifik user med det ID vi fått med
-        if($this->id){
-            $query = "DELETE FROM items
-            WHERE id = $this->id";
-            mysqli_query($connection, $query);
-        }else{
-            echo "No resource given";
-        }
     }
 }
